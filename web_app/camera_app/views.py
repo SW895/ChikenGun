0c56b1d32ea633a3a18cddbc2cbb2e33316dd748
SW895 <stamenkovichnik@gmail.com>
2023-09-25 11:59:21 +0200

    add aaa fun

---
+++ web_app/camera_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def camera_source_view(request):
+    pass
+
+def stream_view(request):
+    pass
+
+def main_view(request):
+    pass
+
+def archive(request):
+    pass
