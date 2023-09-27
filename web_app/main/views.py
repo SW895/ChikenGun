f9f3d93b5655075948599eaf613421d7c4b67982
SW895 <stamenkovichnik@gmail.com>
2023-09-27 16:06:29 +0200

    update archive view

--- web_app/main/views.py
+++ web_app/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import StreamingHttpResponse
-from .utils import gen, VideoCamera
+from .models import ArchiveVideo
+from .forms import Video_created_date
 
 # Create your views here.
 def stream_view(request):
@@ -19,10 +19,23 @@ def main_view(request):
     )
 
 def archive_view(request):
-     return render(
+    if request.method == "POST":
+        form = Video_created_date(request.POST)
+        if form.is_valid():
+            date = form.cleaned_data['date']
+            videos = ArchiveVideo.objects.filter(date_created=date)
+            #objects = form.cleaned_data['objects']
+    else:       
+        date = None
+        #objects = None
+        form = Video_created_date()
+    return render(
         request,
-        'archive_page.html',
+        'archive_page.html',        
         context = {
-            
+           'form':form,
+           'date':date,
+           'videos':videos
+           #'objects':objects,
         }
     )
